e6d2cf8e2702b85a740edcd95e6c35dfee929c99
Vinicius Oliveira <viniciuso.contato@gmail.com>
2021-09-22 01:41:09 -0300

    :wrench: install and setup styled-components

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,7 +1,13 @@
+import { GlobalStyle } from '../../styles/global';
+import * as Styled from './styles';
+
 function Home() {
   return (
     <div className="App">
-      <h1>React Landing Pages</h1>
+      <Styled.Wrapper background="pink">
+        <h1>React Landing Pages</h1>
+      </Styled.Wrapper>
+      <GlobalStyle />
     </div>
   );
 }
