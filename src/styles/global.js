037caa44ebbd08dd7a1de5c47da38af28aedfab4
Vinicius Oliveira <viniciuso.contato@gmail.com>
2021-09-28 14:44:35 -0300

    feat: add Open Sans and Montserrat fonts

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,9 +1,24 @@
-import { createGlobalStyle, css } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%; // 1rem = 10px
+  }
+
+  body {
+    font-size: 1.6rem;
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  h1 {
+    font-family: 'Montserrat', sans-serif;
   }
 `;
