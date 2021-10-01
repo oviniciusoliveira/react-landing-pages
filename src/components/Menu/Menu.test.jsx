2d99135745a322bc0a97d5058e40fe069a42974e
Vinicius Oliveira <viniciuso.contato@gmail.com>
2021-09-30 21:26:36 -0300

    test: add Menu unit tests

---
+++ src/components/Menu/Menu.test.jsx
@@ -0,0 +1,65 @@
+import { renderTheme } from '../../test/render-theme';
+import { Menu } from '.';
+
+import linksMock from '../NavLinks/mock';
+import { fireEvent, screen } from '@testing-library/dom';
+import { theme } from '../../styles/theme';
+const logoData = {
+  text: 'Logo',
+  link: '#target',
+};
+
+describe('<Menu />', () => {
+  test('should render Logo and Main Menu Nav', () => {
+    renderTheme(<Menu links={linksMock} logoData={logoData} />);
+
+    const heading = screen.getByRole('heading', { name: 'Logo' });
+    expect(heading).toBeInTheDocument();
+
+    const navigation = screen.getByRole('navigation', { name: 'Main menu' });
+    expect(navigation).toBeInTheDocument();
+  });
+
+  test('should match snapshot', () => {
+    const { container } = renderTheme(<Menu links={linksMock} logoData={logoData} />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test('should render mobile menu display and open/close menu button', () => {
+    renderTheme(<Menu links={linksMock} logoData={logoData} />);
+
+    const button = screen.getByLabelText('Open or close menu');
+    const menuContainer = button.nextSibling;
+
+    expect(button).toHaveStyleRule('display', 'none');
+    expect(button).toHaveStyleRule('display', 'block', {
+      media: theme.media.ltMedium,
+    });
+
+    expect(menuContainer).toHaveStyleRule('opacity', '0', {
+      media: theme.media.ltMedium,
+    });
+
+    expect(screen.getByLabelText('Open menu')).toBeInTheDocument();
+
+    fireEvent.click(button);
+    expect(menuContainer).toHaveStyleRule('opacity', '1', {
+      media: theme.media.ltMedium,
+    });
+    expect(screen.getByLabelText('Close menu')).toBeInTheDocument();
+
+    fireEvent.click(button);
+    expect(menuContainer).toHaveStyleRule('opacity', '0', {
+      media: theme.media.ltMedium,
+    });
+    expect(screen.getByLabelText('Open menu')).toBeInTheDocument();
+  });
+
+  test('should not render links', () => {
+    const { container } = renderTheme(<Menu logoData={logoData} />);
+    const navigation = screen.getByRole('navigation', { name: 'Main menu' });
+    expect(navigation.firstChild).not.toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+});
--- src/components/NavLinks/index.jsx
+++ src/components/NavLinks/index.jsx
@@ -4,7 +4,7 @@ import { MenuLink } from '../MenuLink';
 
 export const NavLinks = ({ links = [] }) => {
   return (
-    <Styled.NavLinks>
+    <Styled.NavLinks aria-label="Main menu">
       {links.map((link) => (
         <MenuLink key={link.link} {...link} />
       ))}
