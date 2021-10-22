ecd5e56974bd7413a2237c548067650f321f4750
Vinicius Oliveira <viniciuso.contato@gmail.com>
2021-10-22 10:47:35 -0300

    feat: create mapMenuLinks function

--- src/api/map-data.test.js
+++ src/api/map-data.test.js
@@ -7,8 +7,6 @@ describe('map-data', () => {
     expect(pagesData.footerHtml).toBe('');
     expect(pagesData.slug).toBe('');
     expect(pagesData.title).toBe('');
-    expect(pagesData.sections).toEqual([]);
-    expect(pagesData.menu).toEqual({});
   });
 
   test('should map data', () => {
@@ -17,17 +15,11 @@ describe('map-data', () => {
         footer_text: '<p>Hey</p>',
         slug: 'slug',
         title: 'title',
-        sections: ['a', 'b', 'c'],
-        menu: {
-          key: 'value',
-        },
       },
     ])[0];
 
     expect(pagesData.footerHtml).toBe('<p>Hey</p>');
     expect(pagesData.slug).toBe('slug');
     expect(pagesData.title).toBe('title');
-    expect(pagesData.sections).toEqual(['a', 'b', 'c']);
-    expect(pagesData.menu).toEqual({ key: 'value' });
   });
 });
