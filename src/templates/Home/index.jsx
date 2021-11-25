844ca9203b18bb37a8e6ab3756b47fd4ea507650
Vinicius Oliveira <viniciuso.contato@gmail.com>
2021-11-25 19:42:10 -0300

    feat: load pageData from api

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from 'react';
+import { mapData } from '../../api/map-data';
 import { Heading } from '../../components/Heading';
 import * as Styled from './styles';
 
 function Home() {
+  const [pageData, setPageData] = useState([]);
+
+  useEffect(() => {
+    const fetchPage = async () => {
+      const data = await fetch('http://localhost:1337/pages?slug=landing-page');
+      const json = await data.json();
+      const mappedPageData = mapData(json);
+      console.log(mappedPageData);
+      setPageData(mappedPageData[0]);
+    };
+    fetchPage();
+  }, []);
+
+  if (!pageData) {
+    return <h1>Page not found</h1>;
+  }
+
+  if (pageData && pageData.length === 0) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <div className="App">
       <Styled.Wrapper>
