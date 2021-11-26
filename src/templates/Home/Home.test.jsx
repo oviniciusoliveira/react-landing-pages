5d8510d2c80dcaec5ef372d124437b46e22418bb
Vinicius Oliveira <viniciuso.contato@gmail.com>
2021-11-26 01:31:09 -0300

    refactor: remove unused files

--- src/templates/Home/Home.test.jsx
+++
@@ -1,8 +0,0 @@
-import { renderTheme } from '../../test/render-theme';
-import Home from './index';
-
-describe('<Home />', () => {
-  test('should render home', () => {
-    renderTheme(<Home />);
-  });
-});
